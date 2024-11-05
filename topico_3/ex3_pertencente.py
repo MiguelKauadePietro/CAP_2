9ebc55dbaf955f2ef983b160e083082e7902b829
Miguel <lokokkx3@gmail.com>
2024-11-05 15:21:52 -0300

    inserindo o indice do número pertencente da lista de valores

--- topico_3/ex3_pertencente.py
+++ topico_3/ex3_pertencente.py
@@ -2,15 +2,18 @@ lista = [19, 1, 9, 85, 7, 10]
 
 n = int(input("Insira um número para ver se ele pertence à lista: "))
 
+indice = 0
+
 pertence = False
 
 for i in range(len(lista)):
     if n == lista[i]:
         pertence = True
+        indice = i
         break
 
 if pertence:
-    print("Pertence")
+    print(f"Pertence, e está no indice {indice} da lista")
 else:
     print("Não pertence")
 
