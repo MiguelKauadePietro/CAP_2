32e094cd73824c3f04c33c9a1c328bf832286cb8
Miguel <lokokkx3@gmail.com>
2024-11-28 06:26:59 -0300

    tentando resolver o último exercício da lista 3

--- lista_3/ex5_mensagem.py
+++ lista_3/ex5_mensagem.py
@@ -2,10 +2,26 @@ def decodifica_mensagem(str_enorme):
     caracter_bin = []
     char_bin = []
     decodificada = []
+    caracteres = []
+    temp = []
 
     tam = len(str_enorme)
 
-    
+    for i in range(0, tam, 7):
+        char_bin = str_enorme[i:i+7]
+        if char_bin[i] == None:
+            break
+        print(char_bin)
+
+    # tam2 = len(char_bin)
+
+    # for j in range(tam2):
+    #     caracter_bin[j] = ord(char_bin[j])
+    #     decodificada[j] = int(caracter_bin[j], 2)
+    #     caracteres[j] = chr(decodificada[j])
+
+    # return caracteres
+
 
 def testes():
 
@@ -13,7 +29,8 @@ def testes():
     0001110011110010111000111110010110010111101001100001010000011001011
     10100001110100100000111010011010011101010110111111011001101111"""
 
-    print(decodifica_mensagem(texto))
+    # print(len(texto))
 
+    print(decodifica_mensagem(texto))
 
 testes()
