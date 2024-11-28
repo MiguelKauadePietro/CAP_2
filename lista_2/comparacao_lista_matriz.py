640c3f3ad41a45dde1a571d5a1596b9689c3c7a9
Miguel <lokokkx3@gmail.com>
2024-11-28 08:40:28 -0300

    lista 2 resolvida

--- lista_2/comparacao_lista_matriz.py
+++ lista_2/comparacao_lista_matriz.py
@@ -7,10 +7,10 @@ def compara_lista_e_matriz(mat, list):
 
     for i in range(nl):
         for j in range(nc):
-            if mat[i][j] == list[i * nc + j]:
-                return True
+            if mat[i][j] != list[i * nc + j]:
+                return False
 
-            return False
+            return True
 
 
 def testes():
@@ -18,6 +18,12 @@ def testes():
     valores = [[1, 6, 2], [7, 4, 8], [2, 9, 1]]
 
     lista = [1, 6, 2, 7, 4, 8, 2, 9, 1]
+
+    print(compara_lista_e_matriz(valores, lista))
+
+    valores = [[1, 6, 2], [7, 4, 8], [2, 9, 1]]
+
+    lista = [2, 3, 5, 8, 9, 3, 5, 4, 1]
 
     print(compara_lista_e_matriz(valores, lista))
 
