b9d15f6e1b1a810dd5cd74396f8112af633de7a3
Miguel de Pietro <lokokkx3@gmail.com>
2024-12-16 22:59:29 -0300

    resolvendo lista 4

---
+++ C_parte2/lista_4/ex5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    float a, b, c, x1, x2, delta;
+
+    printf("Insira os valores de a, b e c: ");
+    scanf("%f %f %f", &a, &b, &c);
+
+    if (a == 0)
+    {
+        
+    }
+    
+
+
+    return 0;
+}
+
