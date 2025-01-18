e3a761e1a042e00f8dba78a1f5e9284431a41836
MiguelKauadePietro <lokokkx3@gmail.com>
2025-01-17 23:17:10 -0300

    Finalmente terminando a lista 6

--- C_parte2/lista_6/ex7.c
+++ C_parte2/lista_6/ex7.c
@@ -1,44 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#define N 50
 
 
 void substitui_palavra(char texto[], char busca[], char subs[], char res[]){
-	int indice, i = 0, i_apos = 0;
+	int indice, i = 0, i_apos = 0, i_texto = 0;
 
 	indice = strstr(texto, busca) - texto; //posição exata onde a string busca começa
-
-	printf("%d\n", indice);
-
 
 	while(i < indice){
 		res[i] = texto[i];
 		i++;
 	}
 
-	printf("%d\n", i);
-
-
 	for(i_apos = 0; i_apos < strlen(subs); i_apos++){
 		res[indice + i_apos] = subs[i_apos];
 	}
 
-	printf("%d\n", i_apos);
+	i_texto = indice + strlen(busca);
 
-	printf("%s\n", res);
-
-	while(i+i_apos > indice && texto[i] != '\0'){
-		res[i+i_apos] = texto[i+i_apos];
+	while(texto[i] != '\0'){
+		res[i+i_apos] = texto[i_texto];
 		i_apos++;
-		//printf("%c\n", res[i]);
+		i_texto++;
 	}
 
-	res[i] = '\0';
+	res[i_texto] = '\0';
 }
 
 int main(void){
 
-	char text[] = "Ano que vem Paulo ira prestar o vestibular", busca[] = "Paulo", subs[] = "Joao Pedro", res[100];
+	char text[N] = "Ano que vem Paulo ira prestar o vestibular", busca[N] = "Paulo", subs[N] = "Gu Cp viadinho do caraio", res[100];
 
 	substitui_palavra(text, busca, subs, res);
 
