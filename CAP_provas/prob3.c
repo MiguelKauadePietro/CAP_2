d63617cf9759b141ccfec7fe6d36834a8a302ae2
Miguel <lokokkx3@gmail.com>
2025-01-22 23:00:33 -0300

    Aula 21/01/2025

---
+++ CAP_provas/prob3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define N 5
+#define N_USER 10
+
+//Esse programa recebe de entrada n valores(digitados pelo usuário) que irão se repetir ao longo de uma matriz 5x5
+
+
+int main(void){
+
+	
+
+	return 0;
+}
